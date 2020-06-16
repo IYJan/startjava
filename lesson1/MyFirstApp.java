8f2c2e10d86989c6d7b5557d0bf12c6efbd3007e
Jan Loz <tbrn2009@gmail.com>
2020-06-17 02:08:07 +0400

    commit

--- lesson1/MyFirstApp.java
+++ lesson1/MyFirstApp.java
@@ -1,13 +1,5 @@
 public class Variable {
 	public static void main(String[] args) {
-	byte core = 2 ;
-	short short1 = 2147483647;
-	int inteli7 = 2620;
-	long long1 = 9223372036854775807;
-	float freq = 2.6;
-	double rom = 7.91;
-	char char1 = m;
-	boolean boolean1 = true;
-
+		System.out.println("Hello World!");
 	}
 }
--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class Unicode {
 
 	public static void main(String[] args) {
-		
 		for (int i = 33; i <= 126; i++) {
-			System.out.println((char)(i));
+			System.out.println((char)i);
 		}
 	}
 }
