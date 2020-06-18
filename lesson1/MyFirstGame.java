36f5ea9be949073064f6b0bed9663161312a0eb2
Jan Loz <tbrn2009@gmail.com>
2020-06-18 19:18:13 +0400

    commit

--- lesson1/MyFirstGame.java
+++ lesson1/MyFirstGame.java
@@ -1,46 +1,29 @@
 public class MyFirstGame {
-
-		public boolean eql;
-
+	public boolean eql;
 	public static void main (String[] args) {
-
 		long rangeBegin = 0L;
-
 		long rangeEnd = 100L;
-
 		long currentTime = System.currentTimeMillis();
-
 		long randomNumber = rangeBegin + currentTime % (rangeEnd - rangeBegin);
-	
-	
-
 		System.out.println("chislo 1-st random" + randomNumber);
-		
+
 		do {
-				long currentTime2 = System.currentTimeMillis();
-
-				long humNumb = rangeBegin + currentTime2 % (rangeEnd - rangeBegin);
-
-				System.out.println("chislo 2st random" + humNumb);
-
-			if (randomNumber != humNumb) {
-				
+			long currentTime2 = System.currentTimeMillis();
+			long humNumb = rangeBegin + currentTime2 % (rangeEnd - rangeBegin);
+			System.out.println("chislo 2st random" + humNumb);
+				if (randomNumber != humNumb) {
 					eql = false;
 				}
-
-			if (randomNumber == humNumb){
-				System.out.println("Confirmed equality for 2 numbers");		
-				eql = true;
-
-			} else {
-
-				if (randomNumber > humNumb){
-
-					System.out.println("randomNumber > then human num.");		
+				if (randomNumber == humNumb){
+					System.out.println("Confirmed equality for 2 numbers");		
+					eql = true;
 				} else {
-					System.out.println("randomNumber < then human num.");		
-						}
-					} 
-			} while (eql == false);
-}}
-
+					if (randomNumber > humNumb){
+						System.out.println("randomNumber > then human num.");		
+					} else {
+						System.out.println("randomNumber < then human num.");		
+					}
+				}
+		} while (eql == false);
+	}
+}
