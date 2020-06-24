dbd03a636b92b086d2bbf5c133c7ad5942956a19
Jan Loz <tbrn2009@gmail.com>
2020-06-24 10:43:27 +0400

    commit

---
+++ lesson2/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+	String gender = "Man", name = "Mark";
+	byte old = 50, weight = 90;
+	float height = 1.92f;
+
+	public void walk(){};
+	public void sit(){};
+	public void run(){};
+	public void speak(){};
+	public void learnJava(){};
+				
+
+}
