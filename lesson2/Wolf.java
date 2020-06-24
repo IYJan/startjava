1a942f6259cfcf1779946f2e7eeb105294931490
Jan Loz <tbrn2009@gmail.com>
2020-06-24 10:56:21 +0400

    commit

---
+++ lesson2/Wolf.java
@@ -0,0 +1,13 @@
+public class Wolf {
+	String gender = "Man", nickName = "Mark";
+	byte old = 50, weight = 90;
+	String color;
+
+	public void walk(){};
+	public void sit(){};
+	public void run(){};
+	public void howl(){};
+	public void hunting(){};
+				
+
+}
