39bff554a227ed0fd0b22b5979a6fb24a2de2fc4
Jan Loz <tbrn2009@gmail.com>
2020-07-06 12:15:03 +0400

    commit

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,7 +1,25 @@
 public class Wolf {
-	String gender = "Man", nickName = "Mark";
-	byte old = 50, weight = 90;
+	String gender = "Man";
+	String nickName = "Mark";
+	byte old = 50;
+	byte weight = 90;
 	String color = "black";
+
+	public String getGender() {
+		return this.gender;
+	}
+	public String getNickName() {
+		return this.nickName;
+	}
+	public byte getOld() {
+		return this.old;
+	}
+	public byte getWeight() {
+		return this.weight;
+	}
+	public String getColor() {
+		return this.color;
+	}
 
 	void walk() {
 	}
