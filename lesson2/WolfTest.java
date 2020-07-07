3181e8d94db86b3e0c9beb887d1685f42826fbc6
Jan Loz <tbrn2009@gmail.com>
2020-07-07 11:47:53 +0400

    commit

--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -1,15 +1,15 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolf1 = new Wolf();
-		wolf1.age = 44;
-		wolf1.gender = "man";
-		wolf1.nickName = "Li";
-		wolf1.weight = 45;
-		wolf1.color = "black";
-		System.out.println("How old = " + wolf1.age + "\n" + 
-			" NickName = " + wolf1.nickName + "\n" +
-			" Weight = " + wolf1.weight + "\n" +
-			" Color = " + wolf1.color);
+		wolf1.setAge(44);
+		wolf1.setGender("man");
+		wolf1.setNickName("Li");
+		wolf1.setWeight(45);
+		wolf1.setColor("black");
+		System.out.println("How old = " + wolf1.getAge() + "\n" + 
+			" NickName = " + wolf1.getNickName() + "\n" +
+			" Weight = " + wolf1.getWeight() + "\n" +
+			" Color = " + wolf1.getColor());
 
  		wolf1.walk();
 		wolf1.sit();
