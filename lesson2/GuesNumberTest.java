6e37755c962dd37087fb2fe9885308554f35c057
Jan Loz <tbrn2009@gmail.com>
2020-07-11 23:35:38 +0400

    commit

--- lesson2/GuesNumberTest.java
+++ lesson2/GuesNumberTest.java
@@ -1,51 +1,33 @@
 import java.util.Scanner;
 
 public class GuesNumberTest {
+	private static Scanner reader = new Scanner(System.in);
+	private static char sign;
 	public static void main(String[] args) {
-		int randomNumber = (int) (Math.random()*101);
-		Person player1 = new Person();
-		Person player2 = new Person();
-		Scanner reader = new Scanner(System.in);
-		char sign;
-		System.out.println("1-st number for random" + randomNumber);
-		System.out.println("Input Player1, what's u name= ");
-		String name = reader.next();
-		player1.setName(name);
-		System.out.println("Input Player2, what's u name= ");
-		name = reader.next();
-		player2.setName(name);
-		
+		Player player1 = new Player(namePlayer(1));
+		Player player2 = new Player(namePlayer(2));
+		GuesNumber test = new GuesNumber(player1, player2);
+		System.out.println("Player1 name is " + player1.getName());
+		System.out.println("Player2 name is " + player2.getName());
+
 		do {
-			System.out.println(player1.getName() + " what's u number= ");
-			int num = reader.nextInt();
-			player1.setCount(num);
-			do {
-			System.out.println(player2.getName() + " what's u number= ");
-			num = reader.nextInt();
-			player2.setCount(num);
-			if (player1.getCount() == player2.getCount()) {
-				System.out.println(" Your numbers are equality, " + "\n" + 
-								player1.getName() + " = " + player1.getCount() +"\n" + 
-								player2.getName() + " = "+ player2.getCount() + "\n" + 
-								player2.getName() + " u need change the number ");
-			}else break;
-			}while(true);	
-			if (randomNumber == player1.getCount()) {
-				System.out.println(player1.getName() + ": U are WIN " + player1.getCount());
-			} else {
-				System.out.println(player1.getName() + ": U are Wrong ");		
-				if (randomNumber == player2.getCount()) {
-				System.out.println(player2.getName() + ": U are WIN " + player2.getCount());		
-			} else {
-				System.out.println(player2.getName() + ": U are Wrong ");		
-			}
-			}
-			while(true) {
+			test.startPlay();
+		} while (willNext());
+	}
+		private static String namePlayer(int num) {
+			System.out.println("player " + num + ", what's u name: ");
+			String name = reader.next();
+			return name;
+		}
+
+		private static boolean willNext() {
+		do {
 			System.out.println("Do u wanna continue?: Y or N");
 			sign = reader.next().charAt(0);
-			if (sign == 'Y' || sign == 'N') break;
-				else System.out.println("Only Y or N");
-			}
-		} while (sign == 'Y');
-	}
+			if (sign == 'Y') 
+				return true;
+			else if (sign != 'N') System.out.println("Only Y or N"); 
+		} while (sign != 'N');
+		return false;
+		}
 }
