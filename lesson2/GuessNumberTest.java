b75f4d7d9f372eae90cb650bf579313495d440f5
Jan Loz <tbrn2009@gmail.com>
2020-07-16 17:41:58 +0400

    commit

--- lesson2/GuessNumberTest.java
+++ lesson2/GuessNumberTest.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
 	private static Scanner reader = new Scanner(System.in);
-	private static char sign;
+	private static char continue1;
 	public static void main(String[] args) {
-		Player player1 = new Player(isName(1));
-		Player player2 = new Player(isName(2));
-		GuessNumber test = new GuessNumber(player1, player2);
+		Player player1 = new Player(inputName(1));
+		Player player2 = new Player(inputName(2));
+		GuessNumber game = new GuessNumber(player1, player2);
 		System.out.println("Player1 name is " + player1.getName());
 		System.out.println("Player2 name is " + player2.getName());
 
 		do {
-			test.startGame();
+			game.startGame();
 		} while (willNext());
 	}
-		private static String isName(int num) {
+
+		private static String inputName(int num) {
 			System.out.println("player " + num + ", what's u name: ");
-			String name = reader.next();
-			return name;
+			return reader.next();
 		}
 
-		private static boolean willNext() {
+		private static boolean isNext() {
 		do {
 			System.out.println("Do u wanna continue?: Y or N");
-			sign = reader.next().charAt(0);
-			if (sign == 'Y') 
-				return true;
-			else if (sign != 'N') System.out.println("Only Y or N"); 
-		} while (sign != 'N');
+			continue1 = reader.next().charAt(0);
+			if (continue1 == 'Y') {
+		return true;
+			} else { if (continue1 != 'N') System.out.println("Only Y or N"); 
+			}
+		} while (continue1 != 'N');
 		return false;
 		}
 }
