25fc66af61587ac38f72201c363e23c7c6ac762a
Jan Loz <tbrn2009@gmail.com>
2020-07-18 15:41:43 +0400

    commit

--- lesson2/GuessNumberTest.java
+++ lesson2/GuessNumberTest.java
@@ -5,7 +5,7 @@ public class GuessNumberTest {
 	public static void main(String[] args) {
 		System.out.println("player1, what's u name: ");
 		Player player1 = new Player(reader.next());
-		System.out.println("player1, what's u name: ");
+		System.out.println("player2, what's u name: ");
 		Player player2 = new Player(reader.next());
 		GuessNumber game = new GuessNumber(player1, player2);
 
