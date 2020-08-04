b8023b09372a176167380fd96b81c83e7d2bfe22
IYJan <tbrn2009@gmail.com>
2020-08-04 23:23:31 +0400

    lesson_2_3_4

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class GuessNumber {
 	private int randomNumber;
 	private Player player1;
+	private boolean win = false;
 	private Player player2;
 	private static Scanner reader = new Scanner(System.in);
+
 	
 	public GuessNumber(Player player1, Player player2) {
 		this.player1 = player1;
@@ -15,28 +17,51 @@ public class GuessNumber {
 	
 	public void startGame() {
 		randomNumber = (int) (Math.random() * 101);
-		System.out.println("randomNumber = " + randomNumber);
-		while (true) {
-			if (inputNums(player1)) {
+		System.out.println("Подсказка, загадано число: " + randomNumber);
+		cleanArrayNums();
+		int index =0;
+		while (index < 10) {
+			if (inputNumbers(player1, index)) {
 				break;
 			}
-			if (inputNums(player2)) {
+			if (inputNumbers(player2, index)) {
 				return;
 			}
-			break;
+			index++;
+		}
+		if (!win) {
+			System.out.println("Никто не победил");
 		}
 	}
 
-	private boolean inputNums(Player player) {
+	private void cleanArrayNums() {
+		player1.fillNumbers();
+		player2.fillNumbers();
+	}
+
+	private boolean inputNumbers(Player player, int index) {
+		player.setAttemp(index);
+		System.out.println("У игрока осталось " + player.getName() + " " + player.getAttempts() + " попыток");
 		System.out.println(player.getName() + " please, enter u number = ");
-		player.setNumber(reader.nextInt());
-		if (player.getNumber() > randomNumber) {
+		player.setNumbers(reader.nextInt(), index);
+		return isCompareNumbers(player, index);
+	}
+
+	private boolean isCompareNumbers(Player player, int index) {
+		if (player.getNumbers()[index] > randomNumber) {
 			System.out.println(player.getName() + " u number > computer number \n");
-		} else if (player.getNumber() < randomNumber) {
+		} else if (player.getNumbers()[index] < randomNumber) {
 			System.out.println(player.getName() + " u number < computer number \n");
 		} else {
-			System.out.println(player.getName() + " u are win ");
-			return true;
+			System.out.println("Игрок " + player.getName() + " угадал число " + randomNumber + " с " + (index + 1) + " попытки");
+			win = true;
+			if (index == 9 ) {
+				System.out.println("У " + player.getName() + " закончились попытки");
+			}
+				return true;
+		}
+		if (index == 9) {
+			System.out.println("У " + player.getName() + " закончились попытки");
 		}
 		return false;
 	}
