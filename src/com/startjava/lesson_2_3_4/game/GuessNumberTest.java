37e8a9fecaa02370a56d848194ca30579dbeb095
IYJan <tbrn2009@gmail.com>
2020-08-28 02:27:40 +0300

    factoring code lesson_2_3_4

--- src/com/startjava/lesson_2_3_4/game/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumberTest.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class GuessNumberTest {
 	
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		char exit;
 		System.out.println("Hello, player1, what's u name: ");
-		Player player1 = new Player(reader.next());
+		Player player1 = new Player(scan.next());
 		System.out.println("Hello, player2, what's u name: ");
-		Player player2 = new Player(reader.next());
+		Player player2 = new Player(scan.next());
 		GuessNumber game = new GuessNumber(player1, player2);
 
 		do {
 			game.startGame();
 			do {
 				System.out.println("Do u wanna continue?: Y or N");
-				exit = reader.next().charAt(0);
+				exit = scan.next().charAt(0);
 			} while((exit != 'Y') && (exit != 'N'));
 		} while (exit != 'N');
-		reader.close();
+		scan.close();
 	}
 }
